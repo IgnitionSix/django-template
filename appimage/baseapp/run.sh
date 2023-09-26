181514e96fc42ce67fe6ead483fd6905c8fdc93a
Joshua Gooden <joshuag@gmail.com>
2023-09-26 19:59:35 -0400

    Fix some pathing issues, postgres folder map

--- appimage/baseapp/run.sh
+++ appimage/baseapp/run.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 # && python manage.py loaddata 
+sleep 10
 cd baseapp && python manage.py migrate --settings=app.settings.local && python manage.py runserver 0.0.0.0:8000 --settings=app.settings.local
